2667a3705a7feaf141019a92225d63e0d65b26a7
Anjali9293 <70768034+Anjali9293@users.noreply.github.com>
2020-10-31 14:35:59 -0400

    Update script.js
    Added back script code after merge delete

---
+++ public/assets/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+    let cart = getCart();
+
+
+    function getCartArrayFromObject() {
+        return Object.values(cart);
+    }
+
+    function updateCartCount() {
+        let cart = getCartArrayFromObject();
+        if(cart.length > 0) {
+            const cartCount = cart.map(item => {
+                return item.quantity;
+            }).reduce((a, b) => a + b);
+            $('#cartTotal').html(cartCount);
+        }
+    }
+
+    function getCart() {
+        return JSON.parse(localStorage.cart || "{}");
+    }
+
+    updateCartCount();
+});
