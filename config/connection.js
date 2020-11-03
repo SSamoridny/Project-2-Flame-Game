4d11dfd6cd2ef8f73f79e17994efe46e0da22a5f
Niki Fereidooni <nikifereidooni@gmail.com>
2020-11-03 10:17:32 -0500

    Cleaned up files that are not required

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const defaultConnecton = {
     host: 'localhost',
     port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME
+    user: 'root',
+    password: '1booleanbros',
+    database: 'flamegame'
 };
 
 const mysql = require( 'mysql' );
